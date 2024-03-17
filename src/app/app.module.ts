7baa46fe154d0a8061ea98bd740abc8f30a34ef4
Mayra Amaral <mayracunha18@gmail.com>
2024-03-16 21:19:52 -0300

    feat: projects and contact components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { BannerComponent } from './banner/banner.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { AcademicBackgroundComponent } from './academic-background/academic-background.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, BannerComponent, AboutMeComponent, ExperienceComponent, AcademicBackgroundComponent],
+  declarations: [AppComponent, HeaderComponent, BannerComponent, AboutMeComponent, ExperienceComponent, AcademicBackgroundComponent, ProjectsComponent, ContactComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
