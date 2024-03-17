85e9f938003944f736da630c179cbc45893269c2
Mayra Amaral <mayracunha18@gmail.com>
2024-03-16 21:50:12 -0300

    feat: add ssr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +14,9 @@ import { ContactComponent } from './contact/contact.component';
 @NgModule({
   declarations: [AppComponent, HeaderComponent, BannerComponent, AboutMeComponent, ExperienceComponent, AcademicBackgroundComponent, ProjectsComponent, ContactComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [
+    provideClientHydration()
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
