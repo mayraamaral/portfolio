8a57cfbc4126fb0e0419d78fea22ef88a28d5bb1
Mayra Amaral <mayracunha18@gmail.com>
2024-10-18 18:50:59 -0300

    feat: add dark mode and add project item

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  isDarkMode: boolean = false;
+
+  ngOnInit(): void {
+    // Checar se o usuário tem uma preferência armazenada no localStorage
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+      this.isDarkMode = theme === 'dark';
+      this.updateTheme();
+    }
+  }
+
+  toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+    this.updateTheme();
+  }
+
+  updateTheme(): void {
+  const htmlElement = document.documentElement;
+  if (this.isDarkMode) {
+    htmlElement.classList.add('dark');
+  } else {
+    htmlElement.classList.remove('dark');
+  }
+  }
+}
